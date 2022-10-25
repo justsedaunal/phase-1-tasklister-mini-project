d94dc4b241d8f017f0cb18d5632423ee80757cd3
HomeLanderz <86560837+HomeLanderz@users.noreply.github.com>
2022-10-25 12:37:34 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const input = document.querySelector("input#new-task-description").value;
+    const listItem = document.createElement("li");
+    const unorderedList = document.getElementById("tasks");
+    const button = document.createElement("button");
+    button.textContent = "x"
+    listItem.textContent = `${input} `;
+    listItem.appendChild(button);
+    //console.log(listItem);
+    unorderedList.appendChild(listItem);
+    //console.log(unorderedList);
+    button.addEventListener("click", () => {
+      listItem.remove()
+    })
+    form.reset();
+  })
 });
